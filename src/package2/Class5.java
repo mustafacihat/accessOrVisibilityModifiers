390554282f66f132cbdcee1fbc3e51bc8a9da1aa
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2022-06-25 21:45:20 +0200

    Initial commit

---
+++ src/package2/Class5.java
@@ -0,0 +1,14 @@
+package package2;
+
+import package1.Class1;
+
+public class Class5 {
+    public static void main(String[] args) {
+//        Class1 class1 = new Class1();
+//        System.out.println(class1.x);
+//        System.out.println(class1.y);
+//        System.out.println(class1.z);
+//        System.out.println(class1.u);
+//        class1.m();
+    }
+}
